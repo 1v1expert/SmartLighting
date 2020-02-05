772593803fa880c358ae2390d04168225ceef28a
Vladislav <1v1expert@gmail.com>
2020-02-06 02:49:09 +0300

    upd

--- ModbusTCP/management/commands/test_promodem.py
+++ ModbusTCP/management/commands/test_promodem.py
@@ -3,25 +3,14 @@ from ModbusTCP.core.client import ModbusClient
 from ModbusTCP.promodem.app import start
 from ModbusTCP.promodem.client import PromodemClient
 
+
 class Command(BaseCommand):
     help = 'Generates Fake data'
     
     def handle(self, *args, **options):
-        client = PromodemClient(host="192.168.1.41")
-        client.set_brightness(100)
-        signal = client.get_wifi_signal()
-        project_code = client.get_project_code()
-        voltage_inversion = client.get_voltage_inversion()
-        
-        print(signal[0], project_code[0], bool(voltage_inversion[0]))
+        client = PromodemClient(host="192.168.1.41", debug=False)
+        client.set_brightness(0)
+        print(client.get_full_info())
+
         client.set_voltage_inversion(0)
-        voltage_inversion = client.get_voltage_inversion()
-        print(bool(voltage_inversion[0]))
-        # print(type())
-        # start()
-        # https://github.com/sourceperl/pyModbusTCP/tree/master/examples
-        # c = ModbusClient(host="192.168.1.41", port=502, auto_open=True, auto_close=True, timeout=2, debug=True)
-        # c.open()
-        #
-        # c.write_single_register(0, 0)
-        # c.close()
+        print(client.get_full_info())
--- ModbusTCP/promodem/client.py
+++ ModbusTCP/promodem/client.py
@@ -19,25 +19,59 @@ class PromodemClient(object):
         """
         return self._write_command(self.promodem.write_single_register, 0, value)
     
-    def set_voltage_inversion(self, value):
+    def set_voltage_inversion(self, value: int) -> bool:
         """ Установить инверсию напряжения """
         return self._write_command(self.promodem.write_single_register, 1, int(bool(value)))
     
-    def get_wifi_signal(self):
+    def set_threshold_brightness_level(self, value: int) -> bool:
+        """ Установить пороговый уровень яркости для автоматического срабатывания реле """
+        return self._write_command(self.promodem.write_single_register, 2, value)
+    
+    def get_wifi_signal(self) -> int:
         """ Получить уровень принимаемого сигнала WiFi  """
         # return self.promodem.read_coils(14)
-        return self.promodem.read_holding_registers(14)
+        return self.promodem.read_holding_registers(14)[0]
     
-    def get_project_code(self):
+    def get_project_code(self) -> int:
         """ ID: Код проекта  """
-        return self.promodem.read_holding_registers(15)
+        return self.promodem.read_holding_registers(15)[0]
     
-    def get_modification_code(self):
+    def get_modification_code(self) -> int:
         """ ID: Код модификации  """
-        return self.promodem.read_holding_registers(16)
+        return self.promodem.read_holding_registers(16)[0]
     
     def get_voltage_inversion(self):
-        return self.promodem.read_holding_registers(1)
+        voltage_inversion = self.promodem.read_holding_registers(1)
+        return voltage_inversion
+    
+    def get_threshold_brightness_level(self) -> int:
+        """ Установить пороговый уровень яркости для автоматического срабатывания реле """
+        return self.promodem.read_holding_registers(2)[0]
+
+    def get_brightness(self) -> int:
+        """ Получить яркость светильника  % 0…100 % """
+        return self.promodem.read_holding_registers(0)[0]
+    
+    def get_register_values(self) -> list:
+        """ """
+        return [self.get_register_value(0), self.get_register_value(1)]
+    
+    def get_register_value(self, number_register: int) -> dict:
+        return {number_register: self.promodem.read_discrete_inputs(number_register)[0]}
+    
+    def get_brightness_value_when_turned_on(self):
+        return ''
+    
+    def get_full_info(self) -> dict:
+        return {
+            'brightness': self.get_brightness(),
+            'threshold_brightness_level': self.get_threshold_brightness_level(),
+            'voltage_inversion': self.get_voltage_inversion(),
+            'modification_code': self.get_modification_code(),
+            'project_code': self.get_project_code(),
+            'wifi_signal': self.get_wifi_signal(),
+            'register_value': self.get_register_values()
+        }
     
     def _write_command(self, func, *value):
         # open or reconnect TCP to server
