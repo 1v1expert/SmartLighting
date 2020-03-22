1d96cb384ecb85bc2bfd70604f761413274d0de3
Vladislav <1v1expert@gmail.com>
2020-03-22 20:27:12 +0300

    |

--- core/management/commands/init_promodem_db.py
+++ core/management/commands/init_promodem_db.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 from core.models import Promodem
+from django.db import models
 from core.modbustcp.promodem.client import PromodemClient
 
 User = get_user_model()
@@ -19,20 +20,22 @@ promodems = [
 
 def filling_db():
     for promodem in promodems:
-        obj, created = Promodem.objects.get_or_create(ip=promodem["ip"], title=promodem["title"],
-                                                      defaults={"created_by": User.objects.first(),
-                                                                "updated_by": User.objects.first()})
-        if not created:
-            continue
+        try:
+            obj = Promodem.objects.get(ip=promodem["ip"], title=promodem["title"])
+            continue  # not update
+        except models.ObjectDoesNotExist:
+            obj = Promodem(ip=promodem["ip"], title=promodem["title"])
 
         client = PromodemClient(host=promodem["ip"])
         info = client.get_full_info()
+        
         print(info)
         
         for key in info.keys():
             if key == 'register_values':
                 obj.register_values = '%d%d' % (info[key][0], info[key][1])
                 continue
+                
             if getattr(obj, key, None):
                 setattr(obj, key, info[key])
             
