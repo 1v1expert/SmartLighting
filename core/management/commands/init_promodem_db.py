543d93b911975ab1104b7f2267df90960f9c640d
Vladislav <1v1expert@gmail.com>
2020-03-22 05:16:49 +0300

    |

--- core/management/commands/init_promodem_db.py
+++ core/management/commands/init_promodem_db.py
@@ -20,26 +20,27 @@ promodems = [
 def filling_db():
     for promodem in promodems:
         obj, created = Promodem.objects.get_or_create(ip=promodem["ip"], title=promodem["title"],
-                                                      defaults={"created_by": User, "updated_by": User})
+                                                      defaults={"created_by": User.objects.first(),
+                                                                "updated_by": User.objects.first()})
         if not created:
             continue
 
         client = PromodemClient(host=promodem["ip"])
         info = client.get_full_info()
+        print(info)
         
         for key in info.keys():
             if key == 'register_values':
                 obj.register_values = '%d%d' % (info[key][0], info[key][1])
                 continue
-            if getattr(obj, key):
+            if getattr(obj, key, None):
+                setattr(obj, key, info[key])
             
+            obj.save()
             
-        
-        
-
 
 class Command(BaseCommand):
     help = 'Init DB'
     
     def handle(self, *args, **options):
-        pass
+        filling_db()
