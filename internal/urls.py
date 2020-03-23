4489b91c308e6f1ab65e04fe14d0133af0ee0909
Vladislav <1v1expert@gmail.com>
2020-03-23 04:46:10 +0300

    update login/register/logout view

--- internal/urls.py
+++ internal/urls.py
@@ -1,6 +1,6 @@
 # from django.conf.urls import url
-from internal.views import index_view, pages_view
-
+from internal.views import index_view, pages_view, login_view, register_user
+from django.contrib.auth.views import LogoutView
 from django.urls import path, re_path
 
 # app_name = 'internal'
@@ -9,7 +9,7 @@ urlpatterns = [
     # matches any html file
     re_path(r'^.*\.html', pages_view, name='pages'),
     path('', index_view, name='home'),
-    path('login/', index_view, name='login'),
-    path('register/', index_view, name='register'),
-    path('logout/', index_view, name='logout')
+    path('login/', login_view, name='login'),
+    path('register/', register_user, name='register'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
