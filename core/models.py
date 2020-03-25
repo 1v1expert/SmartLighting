c75434c4f50a5d86f82eff33d9d98ff37ae5c54a
Vladislav <1v1expert@gmail.com>
2020-03-25 23:45:07 +0300

    minutes_to_brightness_reset may be null

--- core/models.py
+++ core/models.py
@@ -48,7 +48,7 @@ class Promodem(Base):
     register_values = models.CharField(verbose_name=' Значения однобитовых регистров ', max_length=2, null=True)
     brightness_value_when_turned_on = models.IntegerField(verbose_name='Уровень Яркости 0…100% при включении ', null=True)
     brightness_step = models.IntegerField(verbose_name=' Шаг изменения яркости ', null=True)
-    minutes_to_brightness_reset = models.IntegerField(verbose_name='Количество минут до сброса яркости на дефолт')
+    minutes_to_brightness_reset = models.IntegerField(verbose_name='Количество минут до сброса яркости на дефолт', null=True)
     brightness_after_reset = models.IntegerField(verbose_name='Дефолтная яркость')
     
     class Meta:
