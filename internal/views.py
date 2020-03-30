3b79a196b3e1c71e350e5874aef570be137bf449
Vladislav <1v1expert@gmail.com>
2020-03-30 23:57:48 +0300

    remove

--- internal/views.py
+++ internal/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.forms.utils import ErrorList
 
 from internal.forms import LoginForm, SignUpForm, PromodemForm
 from core.models import Promodem
