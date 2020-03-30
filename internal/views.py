79b42dc40a34771d9c11b8f8d8b3579f399c2be2
Vladislav <1v1expert@gmail.com>
2020-03-30 23:58:12 +0300

    remove one line

--- internal/views.py
+++ internal/views.py
@@ -3,7 +3,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 
 from internal.forms import LoginForm, SignUpForm, PromodemForm
 from core.models import Promodem
